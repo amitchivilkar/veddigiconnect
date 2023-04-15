8ad02e3ec3c1be4376f16a44f94b8d244a2e618f
Amit Chivilkar <amitchivilkar@gmail.com>
2023-04-15 15:20:18 +0530

    change

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -15,7 +15,10 @@ export default function Header() {
         <>
             <header className={styles.header}>
                 <h1 className={styles.logo}><Link href='/'>
-                    <Image src='logo.svg' width={133} height={133} />
+                    <Image 
+                        src='logo.svg' 
+                        width={133} 
+                        height={133} />
                 </Link></h1>
                 <div className={styles.toggle}>
                     <div></div>
@@ -24,10 +27,10 @@ export default function Header() {
                 </div>
                 <nav>
                     <ul className={styles.nav__list}>
-                       {menuList.map((menu) =>(
+                       {menuList.map((menu) => ( 
                         <li key={menu.id}><Link href={menu.href}>{menu.name}</Link></li>
                        ))}
-                       <li className={styles.button}><Link href="/">Let's Connect</Link></li>
+                       <li className={styles.button}><Link href='/'>Let's Connect</Link></li>
                     </ul>
                 </nav>
 
