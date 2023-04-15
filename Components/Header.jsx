93266d8985eebab2ff1549d83c2ff2364f96b1bd
Amit Chivilkar <amitchivilkar@gmail.com>
2023-04-15 16:08:13 +0530

    chnages

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,6 +1,6 @@
-import Link from "next/link"
-import styles from "../styles/Header.module.css"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Header.module.css";
 
 
 export default function Header() {
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from "next/head"
-import Header from "../components/Header"
-import Footer from "../Components/Footer"
+import Head from 'next/head';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
 
 export default function Home() {
   return (
