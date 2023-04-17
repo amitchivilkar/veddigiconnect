563411961ae954aabe17f59c4cb36dd414805943
Amit Chivilkar <amitchivilkar@gmail.com>
2023-04-17 11:21:58 +0530

    pages changes

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,32 @@
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import styles  from '../styles/Page.module.css';
 
 export default function About() {
     return (
         <>
             <Header />
-            <h1>This is about page</h1>
+                <div className={styles.page}>
+                    <header className={styles.heading}>
+                        <h2 className={styles.title}>About</h2> 
+                    </header>
+                    <main className={styles.article}>
+                        <p>Ved Digiconnect founded in 2022 is a Digital Marketing agency based in Mumbai (Maharashtra). We provide Digital Marketing services to our clients. With a major focus on Integrated Digital Marketing, our main aim is to help our clients to market their products & services digitally to the targeted audience which will lead to sales & revenue generation. </p>
+
+                        <p>In today’s digital world, a digital presence is extremely important for any business. We will help businesses to build their brand online by making use of various digital marketing modules like Social Media Ads, SEO, Website Development, and Lead generation to name a few. </p>
+
+                        <p>We will help clients in building trust among the target audience by sharing high-quality & value-added content online on a consistent basis. Trust & authority of a brand leads to natural Sales & we at Ved Digiconnect follow this simple method for digital marketing. </p>
+
+                        <p>We also believe in developing long-lasting relationships with our clients by providing them with excellent online services. Our Team of talented & dedicated individuals having digital marketing expertise will ensure that the quality of service is maintained all the time. We are constantly updating with the latest technological advancements & are committed to delivering our clients the Numero Uno service that will enable them to fulfil their business goals. </p>
+
+                        <h3>Our Vision </h3>
+
+                        <p>We will ensure that our clients make the best use of ever-evolving digital marketing technologies which will enable them to  build stronger online influence & seize growth opportunities in today’s digital world.</p>
+
+                        <h3>Mission: </h3>
+                        <p>We aim to spread happiness & satisfaction among our clients by helping them achieve their financial & personal goals. Through Ved Digiconnect we are thriving to promote professionalism, integrity, and honesty across the planet through the way we conduct our business. </p>
+                    </main>
+                </div>
             <Footer />
         </>
     )
--- pages/services.js
+++ pages/services.js
@@ -1,11 +1,57 @@
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import styles from '../styles/Page.module.css';
 
 export default function Services() {
     return (
         <>  
             <Header />
-            <h1>This is services page</h1>
+            <div className={styles.page}>
+                    <header className={styles.heading}>
+                        <h2 className={styles.title}>About</h2> 
+                    </header>
+                    <main className={styles.article}>
+
+                        <p>Your online presence & social profiles are your form of branding in this digital era This can create numerous benefits for you as a person and also for your business. Personal Branding is the process of marketing yourself and your career or business online. It's about getting people to know who you are, and what are your qualities, skills & expertise. Once they start to like & trust you they will eventually want to work with you or buy from you.  </p>
+
+                        <p>From networking opportunities to finding business leads, to increase your personal creditability, personal branding is extremely essential to succeed in today’s digital world.</p>
+                        
+                        <h3>Website Development</h3>
+
+                        <p>Build your online identity with a user-friendly & smartly designed website. The website provides a solid digital foundation for your business and should be SEO optimized and properly integrated with Social Media. Be it a static website, interactive website,  blogging website, or data-driven E-Commerce Website our team will develop a website as per client & market requirements. </p>
+
+        
+                        <h3>Social Media Marketing -</h3>
+
+                        <p>Social Media Marketing involves promoting your brand online by making use of engaging, informative, exciting & creative content in form of text, creatives, GIFs, Videos, and Reels to name a few. Consistent promotion of your brand on Social Media platforms like Facebook, Instagram, Twitter & Linkedin with high-quality content results in developing brand loyalty among the target audience which in turns lead to your business growth. </p>
+
+                        <p>Our Social Media team consisting of talented, passionate & creative individuals will create a powerful social media presence for your brand.  From planning a social media strategy to creating a content calendar, to making beautiful creatives & videos and pumping it up with Paid Ads we will empower your brand digitally. </p>
+
+                        <h3>Content Management - </h3>
+
+                        <p>If you can visualize it we can create it with our content. Content Marketing is basically making people aware of your brand online by sharing articles, videos & blogs. Also, it is equally important to have Search Engine Optimized content as it drastically improves your website visibility on search engines like google. More visibility leads to more traffic to your website.</p>
+
+                        <h3>SEO - </h3>
+
+                        <p>SEO means Search Engine Optimization. The main purpose of SEO is to optimize your website by ranking it higher in google search results. Higher google rankings lead to more quality traffic to your website which can result in brand recognition & increase in sales.  </p>
+                        <p>When your website is ranking higher in google search, every time someone searches for your product or service, this can boost your sales. Also, If your content is SEO-optimized it will make it easier for your users to find the information they seek and this, in turn, will result in a happy user experience. </p>
+
+                        <p>Most SEO techniques fall into three categories On-Page SEO, Off-Page SEO & Technical SEO. All three techniques are important for SEO Success.</p>
+
+                        <p>On-Page SEO involves tactics related to your website like your website content, title tags, internal linking, URLs, and usage of keywords to name a few. </p>
+
+                        <p>Off-page SEO involves optimization done outside your website like backlinks, social shares, user behavior (time, bounce rate, page/sessions) to name a few Technical SEO involves activities that directly impact how search engines index & crawl your website. Activities include website speed, structured data, site maps & more. </p>
+
+                        <h3>Lead Generation -</h3> 
+                        
+                        <p>This is the most important part of digital marketing. Online lead generation means generating leads online by making use of various lead magnets like landing pages, Social Media Forms, Contests, and takeaways to name a few. Effective lead generation includes a deep understanding of your target audience's interest & mainly the pain points. Once these are fully understood you can develop further strategies that can attract your prospects naturally to your business or service thereby resulting in natural sales.</p>
+
+                        <p>Personal Branding - In today’s digital world when our target audience is mostly available online browsing through social media or searching something on google it is important to build your personal brand online. No matter whether you are a professional having your own business or an actor, sportsperson, or politician, it's of utmost importance to develop your personal identity online.  </p>
+
+                        
+
+                    </main>
+                </div>
             <Footer />
         </>
     )
